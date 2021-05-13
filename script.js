70cdafa0fcd9e629d2fda073dd0fe8fd0f6bb52a
Alex <alex.gyumushyan@gmail.com>
2021-05-12 23:01:53 -0400

    working on fetch

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 //TODO:
 //using fetch contact with the back end
-// 
+
+
+async function displayDefaults(){
+
+    const tableBody = document.getElementById('defaultTable');
+    let dataHtml = ``;
+    const data = {"StockCategory" : "tech"};
+    
+    let listOfStocks = await fetch ('http://localhost:3000/getQuoteRoute/kdjf',
+        {
+            //method: 'GET',
+            body: JSON.stringify(data)
+        }
+    );
+
+    const list = await listOfStocks.json();
+
+    for (i = 0; i < list.Stocks.length; i++){
+        dataHtml += `
+        <thread>
+            <tr>
+                <th>${list.Stocks[i]}</th>
+            </tr>
+        </thread>   
+        `;
+    }
+
+    tableBody.innerHTML = dataHtml;
+}
+
+
+displayDefaults();
